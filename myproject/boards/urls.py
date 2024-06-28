a13508adef65d8e198f01faba519cd7e10db2d9a
danaendraraj05 <danaendraraj@testpress.in>
2024-06-28 12:12:27 +0530

    Implemented Pagination

--- myproject/boards/urls.py
+++ myproject/boards/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
     path('boards/<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/', PostUpdateView.as_view(), name='edit_post'),
-
+    path('boards/<int:pk>/page/<int:page>/', views.board_topics, name='board_topics_paginated'),
 ]
