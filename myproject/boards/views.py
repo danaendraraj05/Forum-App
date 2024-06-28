d023bf424cc410a4cd73767519f3f235a96a74c9
danaendraraj05 <danaendraraj@testpress.in>
2024-06-28 11:42:13 +0530

    Added Edit Option for the post

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -7,7 +7,23 @@ from .models import Board, Topic, Post
 from .forms import PostForm
 from django.db.models import F
 from django.db.models import Count
+from django.views.generic import UpdateView
+from django.utils import timezone
 
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
+    
 @login_required
 def reply_topic(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
